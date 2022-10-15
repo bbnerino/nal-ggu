936f0eef71648c272d0f71535e5d5a1d132e80d8
ihh <imhyunhong@gmail.com>
2022-10-15 23:51:32 +0900

    feat : 헤더추가

--- src/component/main/Header.tsx
+++ src/component/main/Header.tsx
@@ -7,28 +7,34 @@ interface Props{
 const MainHeader = ({location}:Props) => {
   return (
     <Wrapper>
-      <img className='hamburger' src='/assets/hamburger.png'/> 
-      <h1 className='location'>{location}</h1>
+      <div className="header">
+        <img className='hamburger' src='/assets/hamburger.png'/> 
+        <h1 className='location'>{location}</h1>
+      </div>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.nav`
-  position: fixed;
-  top: 0;
-  width: 100%;
-  background-color: #ffffff71;
-  height: 70px;
-  box-shadow:0px 1px 10px #00000029;
-  display: flex;
-  align-items: center;
-  .hamburger{
-    height: 40px;    
-    margin-left: 20px;
-    cursor: pointer;
-  }
-  .location{
-    margin-left: 100px;
+  
+  .header{
+    position: fixed;
+    width: 100%;
+    max-width: 750px;
+    min-width: 350px;
+    background-color: #ffffff71;
+    height: 70px;
+    box-shadow:0px 1px 10px #00000029;
+    display: flex;
+    align-items: center;
+    .hamburger{
+      height: 40px;    
+      margin-left: 20px;
+      cursor: pointer;
+    }
+    .location{
+      margin-left: 100px;
+    }
   }
 `
 
