0db1c251ab9b3e3c71ad83760812cddc30ad06fd
Sorin0404 <haewong27@gmail.com>
2022-10-15 20:20:45 +0900

    feat : reset CSS & theme 적용

---
+++ src/component/setup/Category.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const Category = () => {
+  return (
+    <CategoryContainer>
+      <div>기온</div>
+      <div>비</div>
+      <div>해</div>
+      <div>대기질</div>
+    </CategoryContainer>
+  );
+};
+
+const CategoryContainer = styled.div``;
+
+export default Category;
