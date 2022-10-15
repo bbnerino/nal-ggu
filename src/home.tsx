bad660b373ed9856fd57937c5b715ff34f24ebcd
minsu <cocacolazero024@gmail.com>
2022-10-15 18:00:18 +0900

    Initial Commit

--- src/home.tsx
+++ src/home.tsx
@@ -4,10 +4,11 @@ import './App.css';
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Link to={'/home2'} >
-          <img src='/assets/sun.png' className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <Link to={'/home2'}>
+          <img src='/assets/sun.png' className='App-logo' alt='logo' />
+          <div>Hello</div>
         </Link>
       </header>
     </div>
