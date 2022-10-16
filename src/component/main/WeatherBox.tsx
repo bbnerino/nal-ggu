b032c9c7ca2767997b0262da0cf30eaad2734e1b
이수빈 <snb0768@salelary.com>
2022-10-16 23:51:25 +0900

    feat : add weather data

--- src/component/main/WeatherBox.tsx
+++ src/component/main/WeatherBox.tsx
@@ -1,33 +1,125 @@
-import styled from 'styled-components'
-import { UserWeather, Weather } from '../../type'
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { UserWeather } from "../../type";
 
-interface Props{
-  weather:UserWeather
+interface Props {
+  userWeather: UserWeather;
+  weather: any;
 }
 
-const WeatherBox = ({weather}:Props) => {
+const WeatherBox = ({ userWeather, weather }: Props) => {
+  const [weatherData, setWeatherData] = useState<string>();
+  const [weatherImg, setWeatherImg] = useState<string>();
+
+  const skyTransform = (code: string) => {
+    switch (code) {
+      case "1":
+        setWeatherImg("/assets/icon/sunny.png");
+        return "맑음";
+      case "2":
+        return "구름조금";
+      case "3":
+        return "구름많음";
+      default:
+        return "흐림";
+    }
+  };
+
+  const rainTransform = (code: string) => {
+    switch (code) {
+      case "0":
+        return "없음";
+      case "1":
+        setWeatherImg("/assets/icon/rainy.png");
+        return "비";
+      case "2":
+        return "비/눈";
+      case "3":
+        return "눈";
+      case "4":
+        setWeatherImg("/assets/icon/rainy.png");
+        return "소나기";
+    }
+  };
+
+  useEffect(() => {
+    const CATEGORY = userWeather.category;
+
+    if (weather[CATEGORY]) {
+      switch (CATEGORY) {
+        case "SKY":
+          setWeatherData(skyTransform(weather[CATEGORY].fcstValue));
+          return;
+        case "PTY":
+          setWeatherData(rainTransform(weather[CATEGORY].fcstValue));
+          setWeatherImg("/assets/icon/rainy.png");
+          return;
+        case "TMP":
+          setWeatherData(weather[CATEGORY].fcstValue + "℃");
+          return;
+        case "PCP":
+          if (weather[CATEGORY].fcstValue === "강수없음")
+            setWeatherData(weather[CATEGORY].fcstValue);
+          else setWeatherData(weather[CATEGORY].fcstValue + "mm");
+          setWeatherImg("/assets/icon/rainy.png");
+          return;
+        case "SNO":
+          setWeatherData(weather[CATEGORY].fcstValue + "cm");
+          return;
+        case "POP":
+          setWeatherData(weather[CATEGORY].fcstValue + "%");
+          setWeatherImg("/assets/icon/rainy.png");
+          return;
+        case "REH":
+          setWeatherData(weather[CATEGORY].fcstValue + "%");
+          setWeatherImg("/assets/icon/wet.png");
+          return;
+        case "WAV":
+          setWeatherData(weather[CATEGORY].fcstValue + "M");
+          return;
+        case "VEC":
+          setWeatherData(weather[CATEGORY].fcstValue + "deg");
+          setWeatherImg("/assets/icon/dust.png");
+          return;
+        case "WSD":
+          setWeatherData(weather[CATEGORY].fcstValue + "m/s");
+          setWeatherImg("/assets/icon/dust.png");
+          return;
+        default:
+          return setWeatherData(weather[CATEGORY].fcstValue);
+      }
+    }
+  }, [weather]);
+
   return (
-    <Wrapper size={weather.size===1 ? "40%":"90%"} color={weather.color} >
-      <div>{weather.category}</div>
-      <div>{weather.tilte}°C</div>
+    <Wrapper
+      size={userWeather.size === 1 ? "40%" : "90%"}
+      color={userWeather.color}
+    >
+      <img src={weatherImg} />
+      <div className="weatherTitle">{userWeather.title}</div>
+      <div className="weatherData">{weatherData}</div>
     </Wrapper>
-  )
-}
+  );
+};
 
-interface IweatherBox{
-  size: string,
-  color:string
+interface IweatherBox {
+  size: string;
+  color: string;
 }
 
 const Wrapper = styled.div<IweatherBox>`
   padding: 1rem;
-  background-color: ${props=>props.color};
+  background-color: ${(props) => props.color};
   height: 15rem;
-  width: ${props=>props.size};
+  width: ${(props) => props.size};
   margin: 2.5rem 5%;
   border-radius: 10px;
   font-size: 2rem;
-  h1{
+  .img {
+  }
+
+  h1 {
     font-size: 5rem;
     margin-top: 1.5rem;
   }
@@ -35,7 +127,7 @@ const Wrapper = styled.div<IweatherBox>`
   @media screen and (max-width: 32rem) {
     height: 10rem;
     font-size: 1rem;
-  } 
-`
+  }
+`;
 
-export default WeatherBox
+export default WeatherBox;
