0c8b5369d8d213a9a94855b5f7e95781db586cf9
ejaman <leegm17@naver.com>
2022-10-16 18:00:24 +0900

    wip

--- src/pages/setup/Setup.tsx
+++ src/pages/setup/Setup.tsx
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
 import ColorModal from "../../component/common/ColorModal";
 import ModalFrame from "../../component/common/ModalFrame";
 import Category from "../../component/setup/Category";
 import Header from "../../component/setup/Header";
 import Selected from "../../component/setup/Selected";
+import { dataState } from "../../store/state/example";
 
 const Setup = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [info, setInfo] = useRecoilState(dataState);
+
+  const getData = async () => {
+    const response = await fetch("/data.json", { method: "GET" });
+    const data = await response.json();
+    setInfo(data.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const oncloseModal = () => {
     setIsModalOpen(false);
   };
+
   return (
     <div>
       <Header />
