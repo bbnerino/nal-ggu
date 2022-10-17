c1fb373a0d18a6e8314bac41e746c9f5274f3fdc
이수빈 <snb0768@salelary.com>
2022-10-17 16:03:53 +0900

    style : start page 미세먼지 제거 및 main page 반응형 스타일링

--- src/pages/start/Start.tsx
+++ src/pages/start/Start.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import styled from 'styled-components';
-import StartSelectBox from '../../component/start/startSelectBox';
-import { startState } from '../../store/state/startData';
-import theme from '../../styles/theme';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import StartSelectBox from "../../component/start/startSelectBox";
+import { startState } from "../../store/state/startData";
+import theme from "../../styles/theme";
 
 interface Props {
   title: string;
@@ -17,129 +17,134 @@ const Start = () => {
   const [startData, setStartData] = useRecoilState(startState);
   const presetData = {
     presets: [
-      { id: '1', title: '비가 오는지 궁금해요', icon: '☔', color: '#609FFF' },
+      { id: "1", title: "비가 오는지 궁금해요", icon: "☔", color: "#609FFF" },
       {
-        id: '2',
-        title: '달릴만한 날씨인지 궁금해요',
-        icon: '🏃',
-        color: '#FF7A7A',
+        id: "2",
+        title: "달릴만한 날씨인지 궁금해요",
+        icon: "🏃",
+        color: "#FF7A7A",
       },
-      { id: '3', title: '미세먼지가 궁금해요', icon: '😷', color: '#B470EA' },
-      { id: '4', title: '선크림 발라야 할까요?', icon: '🌞', color: '#FFC42E' },
-      { id: '5', title: '아무렇게나 해줘', icon: '🛏️', color: '#61C3A0' },
+      {
+        id: "3",
+        title: "바람이 많이 부는지 궁금해요",
+        icon: "💨",
+        color: "#B470EA",
+      },
+      { id: "4", title: "선크림 발라야 할까요?", icon: "🌞", color: "#FFC42E" },
+      { id: "5", title: "아무렇게나 해줘", icon: "🛏️", color: "#61C3A0" },
     ],
     selfCustom: [
-      { title: '저는 제가 커스텀할래요', icon: '🌈', color: '#929292' },
+      { title: "저는 제가 커스텀할래요", icon: "🌈", color: "#929292" },
     ],
   };
 
   const initialData = [
     [
       {
-        sort: '비',
-        category: 'POP',
-        title: '강수확률',
-        size: '2',
-        color: '#64B2E3',
+        sort: "비",
+        category: "POP",
+        title: "강수확률",
+        size: "2",
+        color: "#64B2E3",
       },
       {
-        sort: '비',
-        category: 'PCP',
-        title: '1시간 강수량',
-        size: '2',
-        color: '#64B2E3',
-      },
-    ],
-    [
-      {
-        sort: '비',
-        category: 'POP',
-        title: '강수확률',
-        size: '2',
-        color: '#64B2E3',
-      },
-      {
-        sort: '대기',
-        category: 'TMP',
-        title: '1시간 기온',
-        size: '2',
-        color: '#64B2E3',
+        sort: "비",
+        category: "PCP",
+        title: "1시간 강수량",
+        size: "2",
+        color: "#64B2E3",
       },
     ],
     [
       {
-        sort: '대기',
-        category: 'SKY',
-        title: '하늘상태',
-        size: '2',
-        color: '#64B2E3',
+        sort: "비",
+        category: "POP",
+        title: "강수확률",
+        size: "2",
+        color: "#64B2E3",
       },
       {
-        sort: '대기',
-        category: 'TMP',
-        title: '1시간 기온',
-        size: '2',
-        color: '#64B2E3',
-      },
-    ],
-    [
-      {
-        sort: '대기',
-        category: 'REH',
-        title: '습도',
-        size: '2',
-        color: '#64B2E3',
-      },
-      {
-        sort: '비',
-        category: 'POP',
-        title: '강수확률',
-        size: '2',
-        color: '#64B2E3',
+        sort: "대기",
+        category: "TMP",
+        title: "1시간 기온",
+        size: "2",
+        color: "#64B2E3",
       },
     ],
     [
       {
-        sort: '대기',
-        category: 'SKY',
-        title: '하늘상태',
-        size: '2',
-        color: '#64B2E3',
+        sort: "대기",
+        category: "SKY",
+        title: "하늘상태",
+        size: "2",
+        color: "#64B2E3",
       },
       {
-        sort: '대기',
-        category: 'TMP',
-        title: '1시간 기온',
-        size: '2',
-        color: '#64B2E3',
+        sort: "대기",
+        category: "TMP",
+        title: "1시간 기온",
+        size: "2",
+        color: "#64B2E3",
+      },
+    ],
+    [
+      {
+        sort: "대기",
+        category: "REH",
+        title: "습도",
+        size: "2",
+        color: "#64B2E3",
       },
       {
-        sort: '비',
-        category: 'PCP',
-        title: '1시간 강수량',
-        size: '1',
-        color: '#64B2E3',
+        sort: "비",
+        category: "POP",
+        title: "강수확률",
+        size: "2",
+        color: "#64B2E3",
+      },
+    ],
+    [
+      {
+        sort: "대기",
+        category: "SKY",
+        title: "하늘상태",
+        size: "2",
+        color: "#64B2E3",
       },
       {
-        sort: '비',
-        category: 'REH',
-        title: '습도',
-        size: '1',
-        color: '#64B2E3',
+        sort: "대기",
+        category: "TMP",
+        title: "1시간 기온",
+        size: "2",
+        color: "#64B2E3",
       },
       {
-        sort: '바람',
-        category: 'WSD',
-        title: '풍속',
-        size: '1',
-        color: '#64B2E3',
+        sort: "비",
+        category: "PCP",
+        title: "1시간 강수량",
+        size: "1",
+        color: "#64B2E3",
       },
       {
-        sort: '바람',
-        category: 'VEC',
-        title: '풍향',
-        size: '1',
-        color: '#64B2E3',
+        sort: "비",
+        category: "REH",
+        title: "습도",
+        size: "1",
+        color: "#64B2E3",
+      },
+      {
+        sort: "바람",
+        category: "WSD",
+        title: "풍속",
+        size: "1",
+        color: "#64B2E3",
+      },
+      {
+        sort: "바람",
+        category: "VEC",
+        title: "풍향",
+        size: "1",
+        color: "#64B2E3",
       },
     ],
   ];
@@ -147,11 +152,11 @@ const Start = () => {
   const navigate = useNavigate();
 
   const navigateSetup = () => {
-    navigate('/setup');
+    navigate("/setup");
   };
 
   const navigateMain = () => {
-    navigate('/main');
+    navigate("/main");
   };
 
   const setStoreInitialData = (id: string) => {
@@ -169,15 +174,16 @@ const Start = () => {
           <Hello>
             안녕하세요. 저는 <span>날꾸</span>에요! <br />
             <br />
-            저에게 궁금한게 무엇인가요?
-            <br /> 선택해주세요!
+            <span className="SubContent">
+              저에게 궁금한게 무엇인가요? 선택해주세요!
+            </span>
           </Hello>
         </HelloWrapper>
         <BoxWrapper>
           {presetData.presets.map((presetData) => (
             <div
               key={presetData.id}
-              style={{ width: '100%' }}
+              style={{ width: "100%" }}
               onClick={() => {
                 setStoreInitialData(presetData.id);
                 navigateMain();
@@ -192,7 +198,7 @@ const Start = () => {
             </div>
           ))}
           <Divider></Divider>
-          <div style={{ width: '100%' }} onClick={navigateSetup}>
+          <div style={{ width: "100%" }} onClick={navigateSetup}>
             <StartSelectBox
               title={presetData.selfCustom[0].title}
               icon={presetData.selfCustom[0].icon}
@@ -238,6 +244,11 @@ const Hello = styled.div`
   }
   @media screen and (max-width: 32rem) {
     font-size: 1rem;
+  }
+
+  .SubContent {
+    color: black;
+    font-size: 1.5rem;
   }
 `;
 
